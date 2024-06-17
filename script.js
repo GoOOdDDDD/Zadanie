2d4bc81484e390278a3b10b6795b848581af2acb
GoOOdDDDD <yarkir73@mail.ru>
2024-06-17 20:48:55 +0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+var swiper = new Swiper('.swiper-container', {
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false,
+    },
+  });
+  
